cf0dc534b63846590f6afb455745a4bc07ea9f34
cuichao <cuichao01@inspur.com>
2021-10-08 09:01:01 +0800

    新增对离散值的排序编码

--- scorecardpy/discrete.py
+++ scorecardpy/discrete.py
@@ -10,6 +10,7 @@ def discrete_encode(df,col,method='index',lambda_list = []):
     method: 进行处理的方法:
         当前支持的方法:
         index: 按照0-n的方式进行编码，按照数量从多到少进行编码
+        sort: 按照0-n的方式进行编码，按照字符串排序指定 
     lambda_list: 初始化的lambda函数列表,
         [有效的转化参数名称，[其他,lambda 函数 string 字符串],iv]
 
@@ -27,15 +28,22 @@ def discrete_encode(df,col,method='index',lambda_list = []):
     else:
         cols = [cols]
 
-    if method == 'index':
-        for col in cols:
-            distinct_values = df[col].value_counts().index
+    for col in cols:
+        distinct_values = df[col].value_counts().index
+        map_dict = {}
+        if method == 'index':
             map_dict = dict(zip(distinct_values,range(len(distinct_values))))
+        if method == 'sort':
+            map_dict = dict(zip(sorted(distinct_values),range(len(distinct_values))))
+
+        if len(map_dict) < 2:
+            raise Exception('Size of the mapDict is 0 or 1')
             
-            funclist[f'{col}_mapped'] = f'lambda df,pardict: df["{col}"].map(pardict["mapdict"])'
-            parlist[f'{col}_mapped'] = dict() 
-            parlist[f'{col}_mapped']['pars'] = {'mapdict':map_dict}
-            parlist[f'{col}_mapped']['usepar'] = True
+        funclist[f'{col}_mapped'] = f'lambda df,pardict: df["{col}"].map(pardict["mapdict"])'
+        parlist[f'{col}_mapped'] = dict() 
+        parlist[f'{col}_mapped']['pars'] = {'mapdict':map_dict}
+        parlist[f'{col}_mapped']['usepar'] = True
+
     
     lambda_list = transferfunclist(funclist,parlist,lambda_list,multiple=1)
 
